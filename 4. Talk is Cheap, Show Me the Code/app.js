1936ae25ab2a98c345ae9d1396d015a90ac17e7d
imcyber-sector <71095875+imcyber-sector@users.noreply.github.com>
2024-01-23 09:05:46 +0100

    Talk is cheap, show me code creation

---
+++ 4. Talk is Cheap, Show Me the Code/app.js
@@ -0,0 +1,73 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+/**
+ * Header
+ *  - Logo
+ *  - Nav Links
+ * Body
+ *  - Search
+ *  - Restaurant Container
+ *  - RestaurantCard
+ * Footer
+ *  - Copyright
+ *  - Links
+ *  - Address
+ *  - Contact
+ * 
+ */
+
+const styleCard = {
+    backgroundColor: '#f0f0f0'
+};
+
+const Header = () => {
+    return (
+        <div className='header'>
+            <div className='logo-container'>
+                <img className='logo' src='https://www.logodesign.net/logo/smoking-burger-with-lettuce-3624ld.png'/>
+            </div>
+            <div className='nav-items'>
+                <ul>
+                    <li>Home</li>
+                    <li>About Us</li>
+                    <li>Contact Us</li>
+                    <li>Cart</li>
+                </ul>
+            </div>
+        </div>
+    )
+}
+
+const RestaurantCard = () => {
+    return (
+        <div className='restaurant-card' style={styleCard}>
+            <h3>Meghana Foods</h3>
+        </div>
+    )
+}
+
+const Body = () => {
+    return (
+        <div className='body'>
+            <div className='search'>
+                Search
+            </div>
+            <div className='restaurant-container'>
+                <RestaurantCard />
+            </div>
+        </div>
+    )
+}
+
+const AppLayoutComponent = () => {
+    return (
+        <div className='app'>
+            <Header />
+            <Body />
+        </div>
+    )
+}
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<AppLayoutComponent />);
