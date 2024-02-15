1db5501c6cda8a811551f3b89613f25b0b644f12
Fake Jesus <thezitonproject+tools@gmail.com>
2024-02-15 17:28:52 +0100

    episode 8 in progress

--- 8. Namaste React Remaining/src/components/User.js
+++ 8. Namaste React Remaining/src/components/User.js
@@ -5,6 +5,18 @@ const User = ({name}) => {
     const [count, setCount] = useState(0);
     const [count2, setCount2] = useState(1);
 
+    useEffect(() => {
+        // in case you want to do something when count changes
+    }, [count]);
+
+    useEffect(() => {
+        // in case you want to do something when count2 changes
+    }, [count2]);
+
+    // useEffect(() => {
+        // in case you want to do something when either/both of count or count 2 changes
+    // }, [count, count2]);
+
     return (
         <div className="user-card">
             <h1>Count = {count}</h1>
